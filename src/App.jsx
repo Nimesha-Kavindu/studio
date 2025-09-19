2d826f98709a547831d837e692155608348b0e77
Nimesha Kavindu <fc115568@foc.sjp.ac.lk>
2025-09-19 12:33:11 +0530

    add logo

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import logo from './assets/logo.png'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-50 flex flex-col p-4">
+        <img src={logo} alt="Logo" className="h-20 w-auto mx-auto mb-6" />
+    </div>
+  )
+}
+
+export default App
