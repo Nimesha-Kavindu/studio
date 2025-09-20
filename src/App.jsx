2069137bbd70264ea605ba54967e237e10a6870b
Nimesha Kavindu <fc115568@foc.sjp.ac.lk>
2025-09-20 08:05:18 +0530

    finished portfolio page

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
   return (
 
     <BrowserRouter>
-      <div className="min-h-screen bg-gray-50">
+      <div className="min-h-screen bg-white">
         <Routes>
           <Route path='/' element={<Home/>}/>
           <Route path='/my-portfolio' element={<MyPortfolio/>}/>
